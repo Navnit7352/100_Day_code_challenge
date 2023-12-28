89a25eb77d4c697a4569a9ef174e77516a28ad84
Navnit Kumar <143092007+Navnit7352@users.noreply.github.com>
2023-12-28 23:22:48 +0530

    Create KSmallest.java

---
+++ Day_51/KSmallest.java
@@ -0,0 +1,48 @@
+/*
+Given an array arr[] and an integer K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Note :-  l and r denotes the starting and ending index of the array.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+L=0 R=5
+
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7.
+Example 2:
+
+Input:
+N = 5
+arr[] = 7 10 4 20 15
+K = 4 L=0 R=4
+Output : 15
+Explanation :
+4th smallest element in the given 
+array is 15.
+*/
+
+//Code
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        Arrays.sort(arr);
+        int prev=-1;
+        int curr=0;
+        while(curr<k-1){
+           
+            curr++;
+            prev++;
+            if(arr[prev]==arr[curr]){
+                curr++;
+                prev++;
+            }
+        }
+        return arr[curr];
+    } 
+}
