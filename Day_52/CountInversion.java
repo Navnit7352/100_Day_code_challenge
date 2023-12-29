d165235653b8343d0c1d506192f3df9f433675fd
Navnit Kumar <143092007+Navnit7352@users.noreply.github.com>
2023-12-29 23:22:41 +0530

    Create CountInversion.java

---
+++ Day_52/CountInversion.java
@@ -0,0 +1,86 @@
+/*
+Given an array of integers. Find the Inversion Count in the array. 
+
+Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If the array is already sorted then the inversion count is 0.
+If an array is sorted in the reverse order then the inversion count is the maximum. 
+Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+ 
+
+Example 1:
+
+Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+Output: 3
+Explanation: The sequence 2, 4, 1, 3, 5 
+has three inversions (2, 1), (4, 1), (4, 3).
+Example 2:
+
+Input: N = 5
+arr[] = {2, 3, 4, 5, 6}
+Output: 0
+Explanation: As the sequence is already 
+sorted so there is no inversion count.
+Example 3:
+
+Input: N = 3, arr[] = {10, 10, 10}
+Output: 0
+Explanation: As all the elements of array 
+are same, so there is no inversion count.
+*/
+
+//Code
+class Solution
+{
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    //Function to count inversions in the array.
+    static long inversionCount(long arr[], long N)
+    {
+         int n = arr.length;
+        long[] temp = new long[n];
+
+        return mergeSortAndCount(arr, temp, 0, n - 1);
+    }
+     static long mergeSortAndCount(long[] arr, long[] temp, int left, int right) {
+        long count = 0;
+
+        if (left < right) {
+            int mid = (left + right) / 2;
+            count += mergeSortAndCount(arr, temp, left, mid);
+            count += mergeSortAndCount(arr, temp, mid + 1, right);
+            count += mergeAndCount(arr, temp, left, mid + 1, right);
+        }
+
+        return count;
+    }
+    
+    static long mergeAndCount(long[] arr, long[] temp, int left, int mid, int right) {
+        long count = 0;
+
+        int i = left;
+        int j = mid;
+        int k = left;
+
+        while (i <= mid - 1 && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                count += (mid - i);
+            }
+        }
+
+        while (i <= mid - 1) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (i = left; i <= right; i++) {
+            arr[i] = temp[i];
+        }
+
+        return count;
+    }
+}
